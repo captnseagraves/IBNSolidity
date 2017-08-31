ebac62ba8968791950753bd31862ee71f2c9ae2c
Kevin Seagraves <k.s.seagraves@gmail.com>
2017-08-31 17:19:28 -0600

    contract in good shape

--- contracts/TrustAccount.sol
+++ contracts/TrustAccount.sol
@@ -6,36 +6,23 @@ contract TrustAccount is DelayedClaimable {
 
   address public owner;
 
-  /*could have mapping of all trust accounts, or create a new contract for each account. Seems like each should be own contract.*/
-
-/*Data needed:
-1. Owner address - could be creator or designated to other address
-2. Amount to enter into TrustAccount
-3. Date funds will become available
-*/
-
-/*Functions and modifiers needed:
-1.Owner/onlyOwner - OpenZeppelin
-2.Pull funds or send funds - OpenZeppelin
-  - can a contract execute send based on date?
-  - should have onlyAfter modifier, allows user to withdraw funds only after a set date. Shouldn't auto send.
-3.Create TrustAccount - contructor function*/
-
-/*Functionality*/
-
-/*Ownable makes msg.sender owner of the trust */
 
 
-  function TrustAccount(uint256 _startTime, uint256 _endTime, address recipient, uint256 trustValue) public payable {
-    
-    onwer = msg.sender;
 
-    /*setLimits and transferOwnership are onlyOwner functions, Owner may not be set when constructor is called */
+  function TrustAccount(uint256 _startTime, uint256 _endTime, address recipient) public payable {
+
+    owner = msg.sender;
+
     transferOwnership(recipient);
     setLimits(_startTime, _endTime);
 
+    newTrust(this);
   }
 
+  function withdrawFunds() onlyOwner{
+    selfdestruct(owner);
+  }
 
+  event newTrust(address _contractAddress);
 
 }
