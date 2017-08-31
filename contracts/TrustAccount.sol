dbec75931b186a45845346a8ed4c6f092849d997
Kevin Seagraves <k.s.seagraves@gmail.com>
2017-08-31 12:48:03 -0600

    setup, pseudo code for contract

---
+++ contracts/TrustAccount.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.11;
+
+import 'zeppelin-solidity/contracts/token/StandardToken.sol';
+
+contract TrustAccount {
+/*could have mapping of all trust accounts, or create a new contract for each account. Seems like each should be own contract.*/
+
+/*Data needed:
+1. Owner address - could be creator or designated to other address
+2. Amount to enter into TrustAccount
+3. Date funds will become available
+*/
+
+/*Functions and modifiers needed:
+1.Owner/onlyOwner
+2.Pull funds*/
+
+  function TrustAccount() {
+
+  }
+}
