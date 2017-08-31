8f84bc4aa08253046dd4f9592845c6c6867e3bf7
Kevin Seagraves <k.s.seagraves@gmail.com>
2017-08-31 16:00:42 -0600

    added OpenZeppelin, working on contract

--- contracts/TrustAccount.sol
+++ contracts/TrustAccount.sol
@@ -1,9 +1,12 @@
 pragma solidity ^0.4.11;
 
-import 'zeppelin-solidity/contracts/token/StandardToken.sol';
+import 'zeppelin-solidity/contracts/ownership/DelayedClaimable.sol';
 
-contract TrustAccount {
-/*could have mapping of all trust accounts, or create a new contract for each account. Seems like each should be own contract.*/
+contract TrustAccount is DelayedClaimable {
+
+  address public owner;
+
+  /*could have mapping of all trust accounts, or create a new contract for each account. Seems like each should be own contract.*/
 
 /*Data needed:
 1. Owner address - could be creator or designated to other address
@@ -12,10 +15,27 @@ contract TrustAccount {
 */
 
 /*Functions and modifiers needed:
-1.Owner/onlyOwner
-2.Pull funds*/
+1.Owner/onlyOwner - OpenZeppelin
+2.Pull funds or send funds - OpenZeppelin
+  - can a contract execute send based on date?
+  - should have onlyAfter modifier, allows user to withdraw funds only after a set date. Shouldn't auto send.
+3.Create TrustAccount - contructor function*/
 
-  function TrustAccount() {
+/*Functionality*/
+
+/*Ownable makes msg.sender owner of the trust */
+
+
+  function TrustAccount(uint256 _startTime, uint256 _endTime, address recipient, uint256 trustValue) public payable {
+    
+    onwer = msg.sender;
+
+    /*setLimits and transferOwnership are onlyOwner functions, Owner may not be set when constructor is called */
+    transferOwnership(recipient);
+    setLimits(_startTime, _endTime);
 
   }
+
+
+
 }
